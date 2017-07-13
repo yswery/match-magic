4977a31188cc09a8d5091bdfa7ffac117ffc95d1
Yif Swery <yiftachswr@gmail.com>
2017-07-14 11:52:17 +1200

    Upload the memory

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memory;
 use App\Services\Slack;
 use Illuminate\Http\Request;
 
@@ -20,5 +21,16 @@ class UploadController extends Controller
     {
         $image = $request->file('image');
         $text  = $request->input('text');
+
+        // save the image to disk
+        $imageName = '/images/' . time() . '.' . pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION);
+        $image->move(public_path() . $imageName);
+
+        $memory              = new Memory;
+        $memory->image       = $imageName;
+        $memory->description = $text;
+        $memory->save();
+
+        return redirect('/');
     }
 }

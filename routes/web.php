bb7c1eb426e36980ac297a918ae3e6aa83a568fb
Yif Swery <yiftachswr@gmail.com>
2017-07-14 01:32:38 +1200

    Add basic router POST to do reciept upload

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/upload-receipt', function () {
+    // #### THIS GET ENDPOINT WILL NOT EXIST LATER, Should be replaced byt the single page application and only post used
+    return '<form action="/upload-receipt" method="POST" enctype="multipart/form-data">
+              <input type="file" name="receipt" id="receipt">
+              <input type="submit" value="Submit">
+            </form>';
+});
+
+
+Route::post('/upload-receipt', function () {
+    $file = \Illuminate\Support\Facades\Request::file('receipt');
+    $slack = new \App\Services\Slack();
+    return json_encode($slack->sendReceipt('@yif', $file));
 });

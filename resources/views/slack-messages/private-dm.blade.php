af5c9e136691f642c6a59109b91e9b903e61ce9f
Yif Swery <yiftachswr@gmail.com>
2017-07-14 13:02:10 +1200

    Finilsie the private messages with suggestion

--- resources/views/slack-messages/private-dm.blade.php
+++ resources/views/slack-messages/private-dm.blade.php
@@ -1,8 +1,26 @@
-Hey {{ $pair[0]->name }} and {{ $pair[1]->name }}
+Hey guys, congrats! You have both been selected to go have some fun!! :panda_face:
 
-Line break
-*bold* _mark_ ~down~ Testings
+Let’s get this booked in. What are your availabilities for this week?
+Stuck for ideas? Reminisce on past memories here: https://matchmagic.co.nz/
 
-> qoute
+*Here’s some suggestions:*
+- *Food:* {{ $food->description }}
+- *Activity:* {{ $activity->description }}
+- *Random:* {{ $random->description }}
 
-https://media.giphy.com/media/JnZcg39f4Woyk/giphy.gif
+
+_When you have your romantic date, we would love it if you would share your memories here_: https://matchmagic.co.nz/
+
+<?php
+$gifs = [
+    'https://media.giphy.com/media/mksx7SSd2QhP2/giphy.gif',
+    'https://media.giphy.com/media/StroX8p1mkqMo/giphy.gif',
+    'https://media.giphy.com/media/vB6GQ7Ogc6j4I/giphy.gif',
+    'https://media.giphy.com/media/ltMLWLHyjuHkY/giphy.gif',
+    'https://media.giphy.com/media/tEQVq5ASc6CcM/giphy.gif',
+    'https://media.giphy.com/media/l2JeiriCSbb5G5m6s/giphy.gif',
+    'https://media.giphy.com/media/3oKIP6w9AnIcjIre5a/giphy.gif',
+];
+?>
+{{  $gifs[array_rand($gifs)] }}
+

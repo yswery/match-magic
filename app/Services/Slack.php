83c0562bcc19b0837d26f1f48a188123bc386045
Yif Swery <yiftachswr@gmail.com>
2017-07-17 18:48:29 +1200

    Fix the SSL issue by sweep under the rug

--- app/Services/Slack.php
+++ app/Services/Slack.php
@@ -20,6 +20,8 @@ class Slack
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 

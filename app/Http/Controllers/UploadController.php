36d824c72895fbdf11ddbc1a020eba686d712562
Yif Swery <yiftachswr@gmail.com>
2017-08-10 18:59:58 +1200

    Add validation to the controller and view

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,26 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MemoryUpload;
+use App\Http\Requests\ReceiptUpload;
 use App\Models\Memory;
 use App\Services\Slack;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    public function uploadReceipt(Request $request)
+    public function uploadReceipt(ReceiptUpload $request)
     {
-        $file    = $request->file('receipt');
-        $comment = $request->input('comment');
+        $file    = $request->file('receipt_image');
+        $comment = $request->input('receipt_comment');
         $slack   = new Slack();
         $slack->sendReceipt('@hanna', $file, $comment);
 
         return redirect('/');
     }
 
-    public function uploadMemory(Request $request)
+    public function uploadMemory(MemoryUpload $request)
     {
-        $image = $request->file('image');
-        $text  = $request->input('comment');
+        $image = $request->file('memory_image');
+        $text  = $request->input('memory_comment');
 
         // save the image to disk
         $imageName = time() . '.' . pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION);

4445d4e009b1bb72ed976f9780de6b4172a496c5
Yif Swery <yiftachswr@gmail.com>
2017-09-07 18:29:56 +1200

    Add a birthday field

---
+++ database/migrations/2017_09_07_061855_add_birthday_field.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Member;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBirthdayField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->date('birthday')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('birthday');
+        });
+    }
+}
